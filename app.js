f507a23b08d521525ccee8cd8382f6a7f850a655
Nico FOster <nico_foster@hotmail.com>
2020-08-28 19:25:59 -0300

    jugando con efecto 3d

--- app.js
+++ app.js
@@ -19,4 +19,23 @@ navLinks.forEach(link => {
     });
 });
 
+let app = new PIXI.Application({width: 489, height: 694});
+document.body.appendChild(app.view);
 
+let img = new PIXI.Sprite.from("pipper.png");
+
+img.width = 489;
+img.height = 694;
+
+app.stage.addChild(img);
+
+depthMap = new PIXI.Sprite.from("pipper-map.png");
+app.stage.addChild(depthMap);
+
+displacementFilter = new PIXI.filters.DisplacementFilter(depthMap);
+app.stage.filters = [displacementFilter];
+window.onmousemove = function (e) {
+    displacementFilter.scale.x = (window.innerWidth / 2 - e.clientX) / 100;
+    
+    displacementFilter.scale.y = (window.innerHeight / 2 - e.clientY) / 100;
+}
